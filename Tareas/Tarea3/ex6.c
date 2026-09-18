0e5e11b958cffd0baa286ffd6636d6b1f1fe7b47
Baruch R. <azulamarillo000@gmail.com>
2026-09-17 19:00:15 -0600

    ex6 Tarea 3

--- Tareas/Tarea3/ex6.c
+++ Tareas/Tarea3/ex6.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+float error(float *sol, float *b, int dim);
 
 int main(void){
     int n;
-    float **L,*b,*x;
+    float **L,*b,*x,*Lx;
 
+    // Asignación dinámica de memoria
     // printf("\nIngresa n: ");
     scanf("%d",&n);
 
@@ -35,10 +39,20 @@ int main(void){
         for(int k=0;k<n;k++){
             free(L[k]);
         }
-        free(L);
+        free(L); free(b);
         return 1;
     }
 
+    Lx = malloc(n*sizeof(float));
+    if(!Lx){
+        for(int k=0;k<n;k++){
+            free(L[k]);
+        }
+        free(L); free(b); free(x);
+        return 1;
+    }
+
+    // Entradas a consola
     for(int i=0;i<n;i++){
         for(int j=0;j<=i;j++){
             // printf("\nIngresa L %d%d ",i,j);
@@ -51,6 +65,7 @@ int main(void){
         scanf("%f",&b[i]);
     }
 
+    // Solución al sistema Lx = b
     float suma;
     for(int i=0;i<n;i++){
         suma = 0;
@@ -60,10 +75,38 @@ int main(void){
         x[i]=(b[i]-suma)/L[i][i];
     }
 
+    // Salida a consola de la solución Lx = b
     for(int i=0;i<n;i++){
         printf("\n%f ",x[i]);
     }
 
-    free(L); free(b); free(x);
+    // Cálculo del vector Lx
+    for(int i=0;i<n;i++){
+        Lx[i] = 0;
+        for(int j=0;j<=i;j++){
+            Lx[i] += L[i][j] * x[j];
+        }
+    }
+
+    // Salida a consola del error de comprobación
+    float e = error(Lx, b, n);
+    printf("\n%e",e);
+
+    // Liberamos memoria
+    for(int k=0;k<n;k++){
+        free(L[k]);
+    }
+    free(L); free(b); free(x); free(Lx);
     return 0;
+}
+
+float error(float *sol, float *b, int dim){
+
+    float e = 0.0f;
+
+    for(int i=0;i<dim;i++){
+        e += fabsf(sol[i] - b[i]);
+    }
+
+    return e;
 }
