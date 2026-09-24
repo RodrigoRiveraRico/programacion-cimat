4dde411422dffd92d1d5ff752f3ff574645074e0
Baruch R. <azulamarillo000@gmail.com>
2026-09-23 18:54:25 -0600

    idea ex4

---
+++ Tareas/Tarea5/ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b);
+
+int main(void){
+
+    return 0;
+}
+
+int compare(const void *a, const void *b){
+
+    
+}
